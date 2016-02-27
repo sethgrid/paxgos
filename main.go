5169c4a11846d9e64ef552b687aa826ec0b69200
Seth Ammons <seth@sendgrid.com>
2016-02-27 08:41:34 -0800

    reworked from channels to http, a lot more simple

--- main.go
+++ main.go
@@ -1,261 +1,334 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"math/rand"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
-// need to simulate troubled network
-// need to monitor all nodes
-
-// make a pool of machines
-// elect a leader (distinguished leader and learner)
-// elect more distiguished learners for redundancy
-// pass a message to the leader from outside
-// send a proposal to acceptors
-// acceptors ack with promise to not go lower
-// send accept request to acceptors
-// acceptors accept if number is greater than they've seen, notify distinguished learners
-
-type prepareResponse struct {
-	HighestProposalID  int
-	HighestProposalVal int
-	HighestAcceptedID  int
-}
-
-type acceptResponse struct {
-	accepted    bool
-	acceptedID  int
-	acceptedVal int
-}
-
-type request struct {
-	proposalID        int
-	proposalVal       int
-	highestAcceptedID int
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+type node struct{}
 
 var Verbose bool
 
 func main() {
 	var numNodes int
+	var startingPort int
 
 	flag.BoolVar(&Verbose, "v", false, "set flag -v to see verbose logging")
 	flag.IntVar(&numNodes, "nodes", 5, "set the number of nodes. Minimum 3.")
+	flag.IntVar(&startingPort, "starting-port", 9000, "starting with this port, listen on port +1, +2, +3, ..., + number of nodes. A value of 0 will assign random ports.")
 	flag.Parse()
 
 	if numNodes < 3 {
+		log.Printf("Got %d nodes, upping to minimum count of 3", numNodes)
 		numNodes = 3
 	}
 
-	// these are leader specific communication channels and can be shared
-	// in node creation (lets the nodes talk to the leader)
-	inputChan := make(chan int)
-	acceptResponseChan := make(chan acceptResponse)
-	prepareResponseChan := make(chan prepareResponse)
+	log.Printf("Starting %d nodes", numNodes)
 
-	allNodes := make([]*node, 0)
-	for i := 0; i <= numNodes; i++ {
-		// these channels are node specific so a leader or learner can communicate
-		// directly to a given node
-		prepareRequestChan := make(chan request)
-		acceptRequestChan := make(chan request)
-		learnReceiveChan := make(chan acceptResponse)
-		allNodes = append(allNodes, &node{
-			id:                  i,
-			commands:            make(map[int]int),
-			acceptResponseChan:  acceptResponseChan,
-			acceptRequestChan:   acceptRequestChan,
-			prepareResponseChan: prepareResponseChan,
-			prepareRequestChan:  prepareRequestChan,
-			learnReceiveChan:    learnReceiveChan,
-		})
+	// init states
+	acceptors.state = make(map[int]Payload)
+
+	if err := runNodes(numNodes, startingPort); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// index 0 will be the leader
+var ports []string
+
+func runNodes(count, startingPort int) error {
+	ports = make([]string, 0)
+	for i := 0; i <= count; i++ {
+		port := ":0"
+		if startingPort != 0 {
+			port = fmt.Sprintf(":%d", startingPort+i)
+		}
+		l, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ports = append(ports, strings.Trim(l.Addr().String(), "[:]"))
+
+		r := mux.NewRouter()
+
+		// only one proposer, the leader
+		if i == 0 {
+			r.HandleFunc("/propose/{val}", proposeHandler)
+		}
+
+		// acceptor endpoints
+		r.HandleFunc("/prepare/{id}/{val}", prepareHandler)
+		r.HandleFunc("/accept/{id}/{val}", acceptHandler)
+
+		// learner endpoint
+		r.HandleFunc("/learn/{id}/{val}", learnHandler)
+
+		// default
+		r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("root\n")) })
+
+		log.Printf("Starting node on :%s", l.Addr().String())
+		go func(r *mux.Router) {
+			if err := http.Serve(l, r); err != nil {
+				log.Print(err)
+			}
+		}(r)
 	}
 
-	// establish some initial distinguished roles
-	leader := rand.Intn(numNodes)
-	learnerA := leader + 1
-	learnerB := leader + 2
-	if learnerA >= numNodes {
-		learnerA = 0
-	}
-	if learnerB >= numNodes {
-		learnerB = 1
-	}
+	// block
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
 
-	distinguishedLearners := []*node{allNodes[leader], allNodes[learnerA], allNodes[learnerB]}
+	return nil
+}
 
-	vlog("initial setup:\n\tNode Count: %d\n\tLeaderID: %d\n\tDistinguished Learner IDs: %d, %d", numNodes, leader, learnerA, learnerB)
+type Payload struct {
+	NodeID int
+	// phase 1.b acceptor response: I wont promise to anything below this
+	PromiseID int64
+	// phase 1.b acceptor response: this is the highest accepted id I've accepted
+	AcceptedID int64
+}
 
-	for i, _ := range allNodes {
-		if i == leader {
-			go runLeader(allNodes, leader, learnerA, learnerB, inputChan)
+var prepareID int64
+
+func proposeHandler(w http.ResponseWriter, r *http.Request) {
+	prepareChan := make(chan Payload)
+	acceptChan := make(chan Payload)
+
+	// Phase 1.a
+	val := mux.Vars(r)["val"]
+	log.Printf("New proposed value: %v", val)
+	id := atomic.AddInt64(&prepareID, 1)
+
+	for i, _ := range ports {
+		if i == 0 {
+			// don't call the leader. we already are the leader
 			continue
 		}
-		go runNode(allNodes[i], distinguishedLearners, allNodes)
+		go func(i int) {
+			resp, err := http.Get(fmt.Sprintf("http://localhost:%s/prepare/%d/%s", ports[i], id, val))
+			if err != nil {
+				log.Printf("error: phase 1.a - leader GET error with prepare id %d :: %v", id, err)
+				return
+			}
+			defer resp.Body.Close()
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Printf("error: phase 1.a - leader Read error with prepare id %d :: %v", id, err)
+				return
+			}
+			payload := Payload{}
+			err = json.Unmarshal(data, &payload)
+			if err != nil {
+				log.Printf("error: phase 1.a - leader Unmarshal error with prepare id %d :: %v", id, err)
+				return
+			}
+			select {
+			case prepareChan <- payload:
+			case <-time.After(1 * time.Minute):
+				// don't leak goroutines
+				return
+			}
+		}(i)
 	}
 
-	// block for a while
-	<-time.After(1 * time.Hour)
-}
-
-func runLeader(allNodes []*node, leader int, learnerA int, learnerB int, inputChan chan int) {
-	go getInputDataForLeader(inputChan)
-
-	maxStepsAhead := 2
-	highestAcceptedID := -1
-	proposalCounts := make(map[int]int)
-	acceptCounts := make(map[int]int)
-	history := make([]request, 0)
-
-	var proposalID int
-	// sending
-	go func() {
-		for {
-			select {
-			case in := <-inputChan:
-				vlog("leader received input value %d", in)
-				proposalID++
-
-				// track the number of responses we get here
-				proposalCounts[proposalID] = 0
-				acceptCounts[proposalID] = 0
-
-				// don't get too far ahead
-				for proposalID > highestAcceptedID+maxStepsAhead {
-					vlog("blocking proposal id %d > %d + %d", proposalID, highestAcceptedID, maxStepsAhead)
-					<-time.After(1 * time.Second)
-				}
-
-				for _, n := range allNodes {
-					go func(n *node, pID int, pVal int, highestAcceptedID int) {
-						vlog("leader issuing proposal %d (%d) to node id %d", pID, pVal, n.id)
-						n.prepareRequestChan <- request{
-							proposalID:        pID,
-							proposalVal:       pVal,
-							highestAcceptedID: highestAcceptedID,
-						}
-					}(n, proposalID, in, highestAcceptedID)
-				}
-			}
-		}
-	}()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 
 	go func() {
+		counter := 0
 		for {
 			select {
-			case pr := <-allNodes[leader].prepareResponseChan:
-				vlog("leader received prepare response %#v", pr)
-				proposalCounts[pr.HighestProposalID] = proposalCounts[pr.HighestProposalID] + 1
-				if proposalCounts[pr.HighestProposalID] >= ((len(allNodes) / 2) + 1) {
-					for _, n := range allNodes {
-						go func(n *node, pID int, pVal int, highestAcceptedID int) {
-							vlog("leader issuing accept request %d (%d) to node id %d", pID, pVal, n.id)
-							n.acceptRequestChan <- request{
-								proposalID:        pID,
-								proposalVal:       pVal,
-								highestAcceptedID: highestAcceptedID,
-							}
-						}(n, pr.HighestProposalID, pr.HighestProposalVal, highestAcceptedID)
+			case p := <-prepareChan:
+				log.Printf("got prepare response: %#v", p)
+
+				// unusual edge case perhaps, but called for in the paxos algorythm
+				if p.PromiseID > id {
+					// racy. consider atomic all over the place? yuck.
+					id = p.PromiseID
+					// also adjust the prepareID for future requests
+					swapped := atomic.CompareAndSwapInt64(&prepareID, atomic.LoadInt64(&prepareID), p.PromiseID)
+					if !swapped {
+						log.Println("WARNING: unable to swap for larger promise id")
 					}
 				}
-
-			case ar := <-allNodes[leader].acceptResponseChan:
-				vlog("leader received accept response %#v", ar)
-				acceptCounts[ar.acceptedID] = acceptCounts[ar.acceptedID] + 1
-				if acceptCounts[ar.acceptedID] >= ((len(allNodes) / 2) + 1) {
-					// get more accepted than nodes/2 + 1 and we have a winner
-					highestAcceptedID = ar.acceptedID
-					history = append(history, request{proposalID: ar.acceptedID, proposalVal: ar.acceptedVal})
-					vlog("leader sending accept response to distinguished learners: %#v", ar)
-
-					allNodes[learnerA].learnReceiveChan <- ar
-					allNodes[learnerB].learnReceiveChan <- ar
+				counter++
+				if counter > len(ports)/2+1 {
+					wg.Done()
 				}
+			case <-time.After(1 * time.Minute):
+				// don't leak the goroutine
+				return
 			}
 		}
 	}()
 
-	// block a while
-	<-time.After(1 * time.Hour)
+	wg.Wait()
+	// Phase 2.a; we got a majority of responses
+
+	for i, _ := range ports {
+		if i == 0 {
+			// don't call the leader. we already are the leader
+			continue
+		}
+		go func(i int) {
+			resp, err := http.Get(fmt.Sprintf("http://localhost:%s/accept/%d/%s", ports[i], id, val))
+			if err != nil {
+				log.Printf("error: phase 2.a - leader GET error with accept id %d :: %v", id, err)
+				return
+			}
+			defer resp.Body.Close()
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Printf("error: phase 2.a - leader Read error with accept id %d :: %v", id, err)
+				return
+			}
+			payload := Payload{}
+			err = json.Unmarshal(data, &payload)
+			if err != nil {
+				log.Printf("error: phase 2.a - leader Unmarshal error with accept id %d :: %v", id, err)
+				return
+			}
+			select {
+			case acceptChan <- payload:
+			case <-time.After(1 * time.Minute):
+				// don't leak goroutines
+				return
+			}
+		}(i)
+	}
+
+	wg2 := &sync.WaitGroup{}
+	wg2.Add(1)
+
+	go func() {
+		counter := 0
+		for {
+			select {
+			case p := <-acceptChan:
+				log.Printf("got accept response: %#v", p)
+				counter++
+				if counter > len(ports)/2+1 {
+					wg2.Done()
+				}
+			case <-time.After(1 * time.Minute):
+				// don't leak the goroutine
+				return
+			}
+		}
+	}()
+
+	wg2.Wait()
+	w.Write([]byte(fmt.Sprintf("CommandID: %d, Value %v\n", id, val)))
 }
 
-func runNode(n *node, learners []*node, allNodes []*node) {
-	vlog("node id %d starting", n.id)
+var acceptors struct {
+	sync.Mutex
+	// [id] Payload where Payload is the last response sent
+	state map[int]Payload
+}
 
-	promisedID := -1
-	acceptedID := -1
+func prepareHandler(w http.ResponseWriter, r *http.Request) {
+	// Phase 1.b
+	nodeID := determineNodeID(r.Host)
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		log.Println("Node %d: Error in prepare, bad id - %v", nodeID, err)
+	}
+	val, err := strconv.Atoi(vars["val"])
+	if err != nil {
+		log.Println("Node %d: Error in prepare, bad val - %v", nodeID, err)
+	}
+	log.Printf("Node %d: New Prepare [id] value: [%d] %d", nodeID, id, val)
 
-	for {
-		select {
-		case r := <-n.acceptRequestChan:
-			vlog("node id %d accept request %#v", n.id, r)
-			if r.proposalID > acceptedID && r.proposalID >= promisedID {
-				acceptedID = r.proposalID
-				n.acceptResponseChan <- acceptResponse{accepted: true, acceptedID: acceptedID, acceptedVal: r.proposalVal}
-			}
-		case r := <-n.prepareRequestChan:
-			vlog("node id %d prepare request %#v", n.id, r)
-			if r.proposalID > promisedID {
-				promisedID = r.proposalID
-				n.prepareResponseChan <- prepareResponse{HighestAcceptedID: acceptedID, HighestProposalID: promisedID, HighestProposalVal: r.proposalVal}
-			}
-		case r := <-n.learnReceiveChan:
-			vlog("node id %d learning request %#v", n.id, r)
+	acceptors.Lock()
+	defer acceptors.Unlock()
+	state, ok := acceptors.state[nodeID]
+	if !ok {
+		state = Payload{}
+		state.NodeID = nodeID
+	}
+
+	if id < state.PromiseID || id < state.AcceptedID {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	state.PromiseID = id
+	acceptors.state[nodeID] = state
+	err = json.NewEncoder(w).Encode(state)
+	if err != nil {
+		log.Printf("Node %d: error encoding state: %v", err)
+	}
+}
+func acceptHandler(w http.ResponseWriter, r *http.Request) {
+	// Phase 2.b
+	nodeID := determineNodeID(r.Host)
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		log.Println("Node %d: Error in accept, bad id - %v", nodeID, err)
+	}
+	val, err := strconv.Atoi(vars["val"])
+	if err != nil {
+		log.Println("Node %d: Error in accept, bad val - %v", nodeID, err)
+	}
+	log.Printf("Node %d: New accept [id] value: [%d] %d", nodeID, id, val)
+
+	acceptors.Lock()
+	defer acceptors.Unlock()
+	state, ok := acceptors.state[nodeID]
+	if !ok {
+		state = Payload{}
+		state.NodeID = nodeID
+	}
+
+	if id < state.PromiseID || id < state.AcceptedID {
+		w.WriteHeader(http.StatusNoContent)
+		log.Printf("Node %d: command id %d less than promise id %d or accepted id %d", nodeID, id, state.PromiseID, state.AcceptedID)
+		return
+	}
+
+	state.AcceptedID = id
+	acceptors.state[nodeID] = state
+	err = json.NewEncoder(w).Encode(state)
+	if err != nil {
+		log.Printf("Node %d: error encoding state: %v", err)
+	}
+}
+func learnHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	val := vars["val"]
+	log.Printf("New Learn [id] value: [%v] %v", id, val)
+}
+
+// node id is based on location in the ports array due to how we started each node
+func determineNodeID(requestHost string) int {
+	parts := strings.Split(requestHost, ":")
+	if len(parts) != 2 {
+		return -1
+	}
+	port := parts[1]
+	for i, p := range ports {
+		if p == port {
+			return i
 		}
 	}
+	return -1
 }
-
-type node struct {
-	id       int
-	isLeader bool
-
-	acceptResponseChan  chan acceptResponse
-	acceptRequestChan   chan request
-	prepareResponseChan chan prepareResponse
-	prepareRequestChan  chan request
-	learnReceiveChan    chan acceptResponse
-
-	currentProposalID int
-	currentValueID    int
-
-	currentMinAcceptedID     int
-	currentHighestAcceptedID int
-
-	// id to value
-	commands map[int]int
-}
-
-func getInputDataForLeader(inputChan chan int) {
-	// read in data somewhere
-	for i := 0; i <= 10; i++ {
-		vlog("Sending input value %d to leader", i)
-		inputChan <- (i * i)
-	}
-}
-
-func vlog(format string, v ...interface{}) {
-	if Verbose {
-		log.Printf(format, v...)
-	}
-}
-
-/*
-
-Client   Proposer      Acceptor     Learner
-   |         |          |  |  |       |  | --- First Request ---
-   X-------->|          |  |  |       |  |  Request
-   |         X--------->|->|->|       |  |  Prepare(N)
-   |         |<---------X--X--X       |  |  Promise(N,I,{Va,Vb,Vc})
-   |         X--------->|->|->|       |  |  Accept!(N,I,Vm)
-   |         |<---------X--X--X------>|->|  Accepted(N,I,Vm)
-   |<---------------------------------X--X  Response
-   |         |          |  |  |       |  |
-
-*/
